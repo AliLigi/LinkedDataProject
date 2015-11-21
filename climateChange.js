e1703f43913319fb90e911d1e90362e08c63734a
AlinaLigiaDanci <ligiadanci@outlook.com>
2015-11-21 23:35:04 +0000

    New Updates

--- climateChange.js
+++ climateChange.js
@@ -1,19 +1,25 @@
 //Things required to make the API work.
+//======================================================================
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
-
+//======================================================================
 
 //Reading the .json files.
+//======================================================================
 var Emissions = JSON.parse(fs.readFileSync('Emissions.json','utf8'))
 var temperature = JSON.parse(fs.readFileSync('temperature.json','utf8'))
+//======================================================================
 
 //Creating the database.
+//======================================================================
 var db = new sqlite3.Database(':memory:');
 db.serialize(function() {
-    
- // Creating the table for my first database.
+//======================================================================
+	
+// Creating the table for my first database.
 //Inserting the information from the dataset into the table.
+//======================================================================================================================================
     db.run('CREATE TABLE Emissions (Countys,Sector,SulphurDioxide,NitrogenOxide,Ammonia,NonMethaneVolatileOrganicCompound,CarbonMonoxide,LowEmissions,HighEmissions )');
   
     var stmt = db.prepare('INSERT INTO Emissions (Countys,Sector,SulphurDioxide,NitrogenOxide,Ammonia,NonMethaneVolatileOrganicCompound,CarbonMonoxide,LowEmissions,HighEmissions) VALUES (?,?,?,?,?,?,?,?,?)');
@@ -29,9 +35,10 @@ db.serialize(function() {
                , Emissions[i].HighEmissions
               );
   }
-    
+ //======================================================================================================================================   
 // Creating the table for my second database.
 //Inserting the information from the dataset into the table.
+//==============================================================================================================================================
       db.run('CREATE TABLE temperature (Countys,AverageMaximumTemp,AverageMinimumTemp,MeanTemperature,HighestTemp,LowestTemp )');
   
     var stmt = db.prepare('INSERT INTO temperature (Countys,AverageMaximumTemp,AverageMinimumTemp,MeanTemperature,HighestTemp,LowestTemp) VALUES (?,?,?,?,?,?)');
@@ -45,19 +52,21 @@ db.serialize(function() {
               );
   }
   stmt.finalize();
+//===============================================================================================================================================
    
 //Here i am creating inner joins to join the data from the two datasets.
 // My statement is long bevause there is alot of data to be seen from the datasets.
+//Tests table and console.
 
  db.each("SELECT Emissions.*, temperature.* FROM Emissions INNER JOIN temperature ON Emissions.Countys = temperature.Countys", function(err, row) {
       console.log("In " + row.Countys + " due to the SulphurDioxide level: " + row.SulphurDioxide + " , NitrogenOxide level: " + row.NitrogenOxide + " , the Ammonia level: " +row.Ammonia + " , theNonMethaneVolatileOrganicCompound level: " +row.NonMethaneVolatileOrganicCompound + " , the CarbonMonoxide level: " + row.CarbonMonoxide + " , the LowEmissions level: " +row.LowEmissions + " and the HighEmisions level: " + row.HighEmissions + " the AverageMaximumTemp is: " + row.AverageMaximumTemp + " , the AverageMinimumTemp is: " + row.AverageMinimumTemp + " , the MeanTemperature  is:" + row.MeanTemperature + " , the HighestTemp is: " + row.HighestTemp + " and the LowestTemp is: " + row.LowestTemp );
   });
 
 });
-
+//===============================================================================================================================================
 //  Trying to push my data on to the browser at this stage
 // This basically selects the data from the First database and pushes it on the browser 
-
+//===============================================================================================================================================
 var posts = [];
 db.serialize(function() {
     db.each("SELECT * FROM Emissions", function(err, row) {
@@ -65,13 +74,29 @@ db.serialize(function() {
     }, function() {
     })
 })
+//====================================================================================================================================
 
 //db.close();
 var app = express();
+//====================================================================================================================================
 
+//push to browser 
 app.get('/', function(req, res) {
   res.send(posts);
 });
 
+app.get('/accessTemperature', function(req, res){
+	res.send(temperature);
+});
 
+app.get('/delete/:county', function(req, res){
+    console.log("ABOUT TO DELETE THIS DATA");
+    	db.serialize(function(){
+    	db.each('DELETE FROM Emissions WHERE Countys LIKE "' + req.params.Countys + '"',  function(err, row){
+    	console.log("DELETED!");
+    	res.send("DELETED " + req.params.Countys);
+      })
+    })
+	})
+//====================================================================================================================================
 var server = app.listen(8000);
